07eb84af374fb79372445c9415a6d08ff0eafa51
Your Name <letham.5898@gmail.com>
2019-10-02 01:51:33 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+///test 2
+const stringToArray =require('./Array/Array_EX2_sort');
+console.log(stringToArray('12345dhdhdhd6'))
+///test 1
+const sortArray =require('./Array/Array_EX1_string_arr');
+let arr=[3, 8, 7, 6, 5, -4, 3, 2, 1]
+console.log(sortArray(arr))
